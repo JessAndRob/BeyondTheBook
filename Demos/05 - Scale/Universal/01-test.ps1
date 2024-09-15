2b76e98199b8b71ff35778d369ee0b45fa66fe29
jpomfret <jpomfret7@gmail.com>
2024-09-15 09:28:21 +0000

    useful things to knw before we start

--- Demos/05 - Scale/Universal/01-test.ps1
+++ Demos/05 - Scale/Universal/01-test.ps1
@@ -2,3 +2,9 @@
 # as admin
 Install-Module Universal
 Install-PSUServer
+
+# go to the dashboard and look at apis
+# test it there
+# test it here
+Invoke-RestMethod -uri http://localhost:5000/servers
+

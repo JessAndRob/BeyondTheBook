df1400b53a89ec82dfef196a71d67ad18318e660
jpomfret <jpomfret7@gmail.com>
2024-09-15 12:27:47 +0000

    dashboard

--- Demos/05 - Scale/Universal/02-sqlservers-dashboard.ps1
+++ Demos/05 - Scale/Universal/02-sqlservers-dashboard.ps1
@@ -1,51 +1,46 @@
-New-UDApp -Content {
-    Get-UDPage -Name server
+$Pages = @()
+$Pages += New-UDPage -Name 'SQLInstances' -Content {
+    New-UDTypography -Text 'SQL Instances'
 
     New-UDGrid -Container -Content {
         New-UDGrid -Item -ExtraSmallSize 12 -Content {
-            New-UDDataGrid -LoadRows {
-                $Data = Invoke-RestMethod -Uri http://localhost:5000/servers 
-                $Data | Out-UDDataGridData -Context $EventData -TotalRows $Rows.Length
-            } -Columns @(
-                New-UDDataGridColumn -Field SqlInstance -render { 
-                    New-UDLink -Text $EventData.SqlInstance -Url "/server?instance=$($EventData.SqlInstance)"
-                }
-                New-UDDataGridColumn -Field VersionString
-                New-UDDataGridColumn -Field EngineEdition
-                New-UDDataGridColumn -Field Edition
-                New-UDDataGridColumn -Field HostDistribution
-                New-UDDataGridColumn -Field ButtonTest -Render {
-                    New-UDButton -Text "Test" -OnClick {
-                        Show-UDToast -Message "Test"
+
+            $Data = Invoke-RestMethod -Uri http://localhost:5000/SqlInstances/GetSqlInstances
+
+            $Columns = @(
+                New-UDTableColumn -Property SqlInstance -Title "SQL Instance" -render {New-UDLink -Text $EventData.SqlInstance -Url "../db"}
+                New-UDTableColumn -Property VersionString -Title "Version"
+                New-UDTableColumn -Property EngineEdition -Title "Engine Edition"
+                New-UDTableColumn -Property Edition -Title "Edition"
+                New-UDTableColumn -Property HostDistribution -Title "Host Distribution"
+            )
+            New-UDTable -Data $Data -Columns $Columns
+        }
+    }
+}
+$Pages += New-UDPage -Name 'Databases' -url '/db' -Content {
+    New-UDTypography -Text 'Databases'
+
+    $Data = Invoke-RestMethod -Uri "http://localhost:5000/Databases/GetDatabases" -Method Get -Body '{"sqlinstance":"sql1"}' -ContentType 'application/json'
+
+            $Columns = @(
+                New-UDTableColumn -Property SqlInstance -Title "SQL Instance"
+                New-UDTableColumn -Property Name -Title "Name"
+                New-UDTableColumn -Property Status -Title "Status"
+                New-UDTableColumn -Property Compatibility -Title "Compatibility"
+                New-UDTableColumn -Property LastFullBackup -Title "Last Full Backup"
+                New-UDTableColumn -Property LastDiffBackup -Title "Last Diff Backup"
+                New-UDTableColumn -Property LastLogBackup -Title "Last Log Backup"
+                New-UDTableColumn -Property Backup -Render {
+                    New-UDButton -Text "Backup" -OnClick {
+                        Show-UDToast -Message "Starting backup for $($EventData.Name)"
+                        $body = ("`"{0}`":`"{1}`",`"{2}`":`"{3}`"" -f 'SqlInstance', $EventData.SqlInstance, 'Database', $EventData.Name)
+                        Invoke-RestMethod -Uri "http://localhost:5000/Databases/Backup" -Method Post -Body "{ $body }" -ContentType 'application/json'
                     }
                 }
-            ) -AutoHeight $true -AutoSizeColumns $true    
+            )
+            New-UDTable -Data $Data -Columns $Columns
 
-        }
-        New-UDGrid -Item -ExtraSmallSize 3 -Content {
-            New-UDPaper -Content { "xs-3" } -Elevation 2
-        }
-        New-UDGrid -Item -ExtraSmallSize 3 -Content {
-            New-UDPaper -Content { "xs-3" } -Elevation 2
-        }
-        New-UDGrid -Item -ExtraSmallSize 3 -Content {
-            New-UDPaper -Content { "xs-3" } -Elevation 2
-        }
-        New-UDGrid -Item -ExtraSmallSize 3 -Content {
-            New-UDPaper -Content { "xs-3" } -Elevation 2
-        }
-    }
+}
+New-UDApp -Pages $Pages -Title 'SQL Instance Dashboard'
 
-
-} -Navigation (
-    New-UDList -Children {
-        New-UDListItem -Label "Home"
-        New-UDListItem -Label "Getting Started" -Children {
-            New-UDListItem -Label "Installation" -OnClick {}
-            New-UDListItem -Label "Usage" -OnClick {}
-            New-UDListItem -Label "FAQs" -OnClick {}
-            New-UDListItem -Label "System Requirements" -OnClick {}
-            New-UDListItem -Label "Purchasing" -OnClick {}
-        }
-    }
-) -NavigationLayout permanent

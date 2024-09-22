cdc1bcfdb5934871bc9a85fecb250ac63acfe77b
jpomfret <jpomfret7@gmail.com>
2024-09-22 11:05:53 +0000

    working on pu

--- Demos/05 - Scale/Universal/02-sqlservers-dashboard.ps1
+++ Demos/05 - Scale/Universal/02-sqlservers-dashboard.ps1
@@ -1,11 +1,19 @@
 $Pages = @()
+$Pages += New-UDPage -Name 'Home' -Content {
+    New-UDTypography -Text 'Home'
+
+    $instances = Invoke-RestMethod -Uri http://localhost:5000/SqlInstances/GetSqlInstances
+
+    $databases
+
+} -Icon home
 $Pages += New-UDPage -Name 'SQLInstances' -Content {
     New-UDTypography -Text 'SQL Instances'
 
     New-UDGrid -Container -Content {
         New-UDGrid -Item -ExtraSmallSize 12 -Content {
 
-            $Data = Invoke-RestMethod -Uri http://localhost:5000/SqlInstances/GetSqlInstances
+            $instances = Invoke-RestMethod -Uri http://localhost:5000/SqlInstances/GetSqlInstances
 
             $Columns = @(
                 New-UDTableColumn -Property SqlInstance -Title "SQL Instance" -render {New-UDLink -Text $EventData.SqlInstance -OnClick {
@@ -17,16 +25,42 @@ $Pages += New-UDPage -Name 'SQLInstances' -Content {
                 New-UDTableColumn -Property Edition -Title "Edition"
                 New-UDTableColumn -Property HostDistribution -Title "Host Distribution"
             )
-            New-UDTable -Data $Data -Columns $Columns
+            New-UDTable -Data $instances -Columns $Columns
         }
     }
 } -Icon "fas fa-server"
 $Pages += New-UDPage -Name 'Databases' -url '/databases' -Content {
-    New-UDTypography -Text ('Databases for {0}' -f $page:instance) -Variant "h3" 
-    $body = [PSCustomObject]@{ SqlInstance = $page:instance } | ConvertTo-Json
-    $Data = Invoke-RestMethod -Uri "http://localhost:5000/Databases/GetDatabases" -Method Get -Body $body -ContentType 'application/json'
 
-           $Columns = @(
+    $instances = Invoke-RestMethod -Uri http://localhost:5000/SqlInstances/GetSqlInstances
+
+    if (-not $page:instance) {
+        New-UDTypography -Text ('Databases') -Variant "h3" 
+    } else {
+        New-UDTypography -Text ('Databases for {0}' -f $page:instance) -Variant "h3" 
+    }
+    New-UDStack -Content {
+        New-UDPaper -Content {
+            New-UDTypography -Text 'Select an instance to view the databases for it'
+        }
+        New-UDPaper -Content {
+            New-UDSelect -Option {
+                $instances.SqlInstance.foreach{
+                    New-UDSelectOption -Name $_ -Value $_
+                }
+            } -OnChange {
+                Show-UDToast -Message $EventData
+                $page:instance = $EventData
+                Sync-UDElement -Id 'databases'
+            }
+        }
+    } -Direction 'column'
+
+    New-UDDynamic -Id 'databases' -Content {
+        if ($page:instance) {
+            $body = [PSCustomObject]@{ SqlInstance = $page:instance } | ConvertTo-Json
+            $Data = Invoke-RestMethod -Uri "http://localhost:5000/Databases/GetDatabases" -Method Get -Body $body -ContentType 'application/json'
+
+            $Columns = @(
                 New-UDTableColumn -Property SqlInstance -Title "SQL Instance"
                 New-UDTableColumn -Property Name -Title "Name"
                 New-UDTableColumn -Property Status -Title "Status"
@@ -41,12 +75,16 @@ $Pages += New-UDPage -Name 'Databases' -url '/databases' -Content {
                         $backup = Invoke-RestMethod -Uri "http://localhost:5000/Databases/Backup" -Method Post -Body "{ $body }" -ContentType 'application/json'
                         if($backup.BackupComplete) {
                             Show-UDToast -Duration 3000 -Icon "fas fa-copy" -Message "Backup completed successfully for $($EventData.Name) at $($backup.End)."
+                        } else {
+                            Show-UDToast -Duration 3000 -Icon "fas fa-times" -Message "Backup failed!"
                         }
+                        Sync-UDElement -Id 'databases'
                     }
                 }
-           )
-          New-UDTable -Data $Data -Columns $Columns -ShowPagination -PageSize 10
-
+            )
+            New-UDTable -Data $Data -Columns $Columns -ShowPagination -PageSize 10
+        }
+    }
 } -Icon "fas fa-database"
 
 New-UDApp -Pages $Pages -Title 'SQL Instance Dashboard'

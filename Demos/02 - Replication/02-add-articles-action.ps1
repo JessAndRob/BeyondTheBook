5a7479dc3d94f66c5de8884dea6c465f78c65ef9
jpomfret <jpomfret7@gmail.com>
2025-02-09 14:20:21 +0000

    updates to repl demos

--- Demos/02 - Replication/02-add-articles-action.ps1
+++ Demos/02 - Replication/02-add-articles-action.ps1
@@ -4,16 +4,18 @@
 https://github.com/organizations/JessAndRob/settings/actions/runners/new?arch=x64&os=win
 
 # Create a folder under the drive root
-mkdir actions-runner; cd actions-runner
+Set-Location C:\
+New-Item -Path actions-runner -ItemType Directory
+Set-Location actions-runner
 
 # Download the latest runner package
-Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v2.319.1/actions-runner-win-x64-2.319.1.zip -OutFile actions-runner-win-x64-2.319.1.zip
+Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v2.322.0/actions-runner-win-x64-2.322.0.zip -OutFile actions-runner-win-x64-2.322.0.zip
 
 # Optional: Validate the hash
-if((Get-FileHash -Path actions-runner-win-x64-2.319.1.zip -Algorithm SHA256).Hash.ToUpper() -ne '1c78c51d20b817fb639e0b0ab564cf0469d083ad543ca3d0d7a2cdad5723f3a7'.ToUpper()){ throw 'Computed checksum did not match' }
+if((Get-FileHash -Path actions-runner-win-x64-2.322.0.zip -Algorithm SHA256).Hash.ToUpper() -ne 'ace5de018c88492ca80a2323af53ff3f43d2c82741853efb302928f250516015'.ToUpper()){ throw 'Computed checksum did not match' }
 
 # Extract the installer
-Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-2.319.1.zip", "$PWD")
+Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-2.322.0.zip", "$PWD")
 
 # Create the runner and start the configuration experience
 ./config.cmd --url https://github.com/JessAndRob --token ** # get this from the url above
@@ -31,6 +33,10 @@ code .\.github\ISSUE_TEMPLATE\AddArticle.yaml
 code .\.github\workflows\replication.yaml
 
 ## GO CREATE AN ISSUE and watch the workflow
+    # Database: AdventureWorksLT2022
+    # Publication: testPub
+    # Schema: SalesLT
+    # Article: Product
 
 ## check on the articles
 Get-DbaReplArticle -SqlInstance sql1 -Publication testPub | Format-Table

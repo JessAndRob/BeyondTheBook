623d65fd718ffa0c8e0ad0c75047edbcde92902d
jpomfret <jpomfret7@gmail.com>
2025-02-13 12:12:46 +0000

    added a random log shipping demo

---
+++ Demos/06 - What Do You want/LogShippingMigration.ps1
@@ -0,0 +1,111 @@
+## This is a demo to setup log shipping from one source to 2 destinations, with the goal of then joining them to an AG
+
+# get a database
+
+Invoke-WebRequest -Uri https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2017.bak -OutFile \\$source\c$\temp\AdventureWorks2017.bak
+Restore-DbaDatabase -SqlInstance $source -Path C:\temp\AdventureWorks2017.bak -UseDestinationDefaultDirectories
+
+$source = 'sql3'
+$databaseName = 'AdventureWorks2017'
+$secondNode = 'sql1'
+$thirdNode = 'sql2'
+$sharedPath = '\\sql1\Backups\'
+
+# Full backup
+$backupParams = @{
+    SqlInstance = $source
+    Database = $databaseName
+    Type = 'Full'
+    CopyOnly = $false
+    CompressBackup = $true
+    Path = $sharedPath
+    OutVariable = 'fullBackup'
+}
+Backup-DbaDatabase @backupParams
+
+## restore norecovery to log shipping destinations
+$restoreParams = @{
+    Path = $fullBackup.BackupPath
+    NoRecovery = $true
+    UseDestinationDefaultDirectories = $true
+}
+$secondNode, $thirdNode | ForEach-Object { 
+    Restore-DbaDatabase -SqlInstance $_ @restoreParams
+} 
+
+# setup
+$params = @{
+    SourceSqlInstance = '$source'
+    DestinationSqlInstance = '$secondNode', '$thirdNode'
+    Database = 'AdventureWorks2017'
+    SharedPath = '\\sql1\Backups\sql3'
+    CopyDestinationFolder = '\\sql1\Backups\sql2'
+    CompressBackup = $true
+    GenerateFullBackup = $false  # do we want this
+    BackupScheduleStartTime = '000000'
+    CopyScheduleStartTime = '000500'
+    RestoreScheduleStartTime = '001000'
+    Force = $true
+}
+Invoke-DbaDbLogShipping @params
+
+## cutover
+
+# disable jobs (just log shipping here - are there other jobs to disable?)
+Get-DbaAgentJob -SqlInstance $secondNode,$thirdNode,$source -Category 'log shipping' | Set-DbaAgentJob -Disabled
+
+# run log shipping jobs once more 
+#backup
+Start-DbaAgentJob -SqlInstance $source -Job ('LSBackup_{0}' -f $databaseName) -Wait
+
+# copy
+Start-DbaAgentJob -SqlInstance $secondNode -Job ('LSCopy_{0}_{1}' -f $source, $databaseName) -Wait
+Start-DbaAgentJob -SqlInstance $thirdNode -Job ('LSCopy_{0}_{1}' -f $source, $databaseName) -Wait
+
+# restore
+Start-DbaAgentJob -SqlInstance $secondNode -Job ('LSRestore_{0}_{1}' -f $source, $databaseName) -Wait
+Start-DbaAgentJob -SqlInstance $thirdNode -Job ('LSRestore_{0}_{1}' -f $source, $databaseName) -Wait
+
+# drop log shipping configuration - but keep the secondary databases
+$params = @{
+    PrimarySqlInstance = $source
+    SecondarySqlInstance = $secondNode
+    Database = $databaseName
+    RemoveSecondaryDatabase = $false
+}
+Remove-DbaDbLogShipping @params
+
+## clean up jobs from thirdNode
+Get-DbaAgentJob -SqlInstance $thirdNode -Category 'Log Shipping' | Remove-DbaAgentJob -Confirm:$false
+
+# final log backup from source
+$backupParams = @{
+    SqlInstance = $source
+    Database = $databaseName
+    Path = '\\sql1\Backups\sql3' #TODO: CHANGE PATH
+    Type = 'Log'
+    CompressBackup = $true
+    OutVariable = 'trnBackup'
+}
+Backup-DbaDatabase @backupParams
+
+# restore that log file with continue (can nodes get here? otherwise we need to move it to their folder)
+$restoreParams = @{
+    SqlInstance = $thirdNode
+    DatabaseName = $databaseName
+    Path = $trnBackup.Path
+    Continue = $true
+    NoRecovery = $false # bring online on node 2
+}
+Restore-DbaDatabase @restoreParams
+
+$restoreParams = @{
+    SqlInstance = $secondNode
+    DatabaseName = $databaseName
+    Path = $trnBackup.Path
+    Continue = $true
+    NoRecovery = $true # leave it in norecovery on node 3 for AG join
+}
+Restore-DbaDatabase @restoreParams
+
+# go add to AG in GUI

e22453fdb5db9361f4a241161cbc4918440979bd
jpomfret <jpomfret7@gmail.com>
2025-09-24 18:35:01 +0000

    fixes for dab

--- Demos/09 - Data API Builder/dab-demo.ps1
+++ Demos/09 - Data API Builder/dab-demo.ps1
@@ -9,14 +9,18 @@ dotnet tool install --global Microsoft.DataApiBuilder
 # either locally or in a container
 docker run -p 2500:1433 --volume shared:/shared:z --name mssql1 --hostname mssql1 -d dbatools/sqlinstance
 
+# we have sql1 to use
+Get-DbaDatabase -SqlInstance sql1 -ExcludeSystem | Select-Object Name
+
 # Create the configuration
 # sql1
-dab init --database-type "mssql" --host-mode "Development" --connection-string "Server=sql1;User Id=sqladmin;Database=AdventureWorks2022;Password=dbatools.IO;TrustServerCertificate=True;Encrypt=True;"
+dab init --database-type "mssql" --host-mode "Development" --connection-string "Server=sql1;User Id=sqladmin;Password=dbatools.IO!;Database=AdventureWorks2022;TrustServerCertificate=True;Encrypt=True;"
+
 # Container
 dab init --database-type "mssql" --host-mode "Development" --connection-string "Server=localhost,2500;User Id=sqladmin;Database=pubs;Password=dbatools.IO;TrustServerCertificate=True;Encrypt=True;"
 
 # add an entity (case sensitive entity names)
-dab add Author --source "dbo.authors" --permissions "anonymous:*"
+dab add SalesPerson --source "Sales.SalesPerson" --permissions "anonymous:*"
 
 # view the config file
 code dab-config.json
@@ -27,34 +31,34 @@ dab start
 # go view the sites
 # "http://localhost:5000" # shows it's healthy
 # "http://localhost:5000/swagger" # shows the swagger page
-# "http://localhost:5000/api/author" # shows the data
+# "http://localhost:5000/api/SalesPerson" # shows the data
 
 # open another terminal
 
 # get data from PowerShell
-(Invoke-RestMethod -Uri "http://localhost:5000/api/Author").Value | Format-Table
+(Invoke-RestMethod -Uri "http://localhost:5000/api/SalesPerson").Value | Format-Table
 
 # count the records
-(Invoke-RestMethod -Uri "http://localhost:5000/api/Author").Value | Measure-Object
+(Invoke-RestMethod -Uri "http://localhost:5000/api/SalesPerson").Value | Measure-Object
 
 # insert data from PowerShell
 $body = @{
-    au_id = "999-99-9999"
-    au_lname = "Smith"
-    au_fname = "John"
-    phone = "555-555-5555"
-    address = "123 Main St"
-    city = "Anytown"
-    state = "CA"
-    zip = "12345"
-    contract = $false
+      "BusinessEntityID" = 4
+      "TerritoryID" = $null
+      "SalesQuota" = $null
+      "Bonus" = 0
+      "CommissionPct" = 0
+      "SalesYTD" = 559697.5639
+      "SalesLastYear" = 0
+      "rowguid" = "76bacc71-1b2e-4573-9c74-b0badafa6816"
+      "ModifiedDate" = $(get-date)
 } | ConvertTo-Json
-Invoke-RestMethod -Uri "http://localhost:5000/api/Author" -Method Post -Body $body -ContentType "application/json"
+Invoke-RestMethod -Uri "http://localhost:5000/api/SalesPerson" -Method Post -Body $body -ContentType "application/json"
 
 # verify the insert
-(Invoke-RestMethod -Uri "http://localhost:5000/api/Author").Value | Format-Table
+(Invoke-RestMethod -Uri "http://localhost:5000/api/SalesPerson").Value | Format-Table
 
-(Invoke-RestMethod -Uri "http://localhost:5000/api/Author").Value | Measure-Object
+(Invoke-RestMethod -Uri "http://localhost:5000/api/SalesPerson").Value | Measure-Object
 
 # Next steps
 # Deploy to a container app\instance in Azure
